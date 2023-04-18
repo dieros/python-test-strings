5b37f735f83adc1c1a7f3d374ed3f5f253b596bc
drosenfeld <diegorosen@gmail.com>
2023-04-18 19:27:17 -0300

    Add tests to check algorithm works correctly.

---
+++ tests/test_solution.py
@@ -0,0 +1,15 @@
+import pytest
+from solution.solution import solution
+
+
+def test_one_digram_long_string():
+    assert solution('aakmaakmakda') == 7
+    
+
+def test_one_digram_short_string():
+    assert solution('aaa') == 1
+    
+
+def test_no_digram():
+    assert solution('something') == -1
+    
